632e7758e42cef16752368f79ad42222864bfa36
David <olayinkadavid62@yahoo.com>
2022-12-24 05:21:11 +0100

    Added one more project to the react project tab

--- src/Components/Projecttab2.js
+++ src/Components/Projecttab2.js
@@ -2,14 +2,14 @@ import React from "react";
 import '../style.css';
 import img1 from "../assets/images/Restaurant.png";
 import img6 from "../assets/images/portfolio.png";
-
-
+import img7 from "../assets/images/localhost_3000_(Nest Hub Max).png";
 
 export const Projecttab2 = () =>{
     return(
         <div className="Projecttab">
             <a href="https://main--seundavegericht.netlify.app/"><img src={img1} alt="img1" className="img1" /></a>
             <a href="https://seundaveportfolio.netlify.app/"><img src={img6} alt="img1" className="img1" /></a>
+            <a href="https://seundave-tesla.netlify.app//"><img src={img7} alt="img1" className="img1" /></a>
         </div>
     )
 }
